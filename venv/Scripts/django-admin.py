7f80a6a9da901d7768e54dc0a102725598b56b18
Viktor Košťan <viktor.kostan@gmail.com>
2016-07-19 19:14:04 +0200

    Virtual environment

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\viktoris\documents\my_stuff\projects\datalump\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
